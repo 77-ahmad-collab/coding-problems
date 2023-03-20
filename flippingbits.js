746ace1671628fc8b0aad3b02b5a5b2eaa497260
77-ahmad-collab <ahmaq098@gmail.com>
2023-03-20 12:48:59 +0500

    flipping bits challenge solved

--- flippingbits.js
+++ flippingbits.js
@@ -3,12 +3,8 @@ const numberIsZeroOrOne = (currentNumber) => {
   else return false;
 };
 
-const convertDecimalTo32BitBinary = (n) => {
-  let remainder = 0;
-  let currentNumber = n;
-  const integerBits = 32;
-  let isBreak = false;
-  let binaryNumber = "";
+const convertDecimalToBinary = (n) => {
+  let [remainder,currentNumber,isBreak,binaryNumber]=[0,n,false,""]
 
   while (!isBreak) {
     const quotient = Math.floor(currentNumber / 2);
@@ -21,16 +17,42 @@ const convertDecimalTo32BitBinary = (n) => {
     currentNumber = quotient;
   }
 
-  binaryNumber = "0".repeat(integerBits - binaryNumber.length) + binaryNumber;
-
   return binaryNumber;
 };
 
-const flipBits = (n) => {
+const convertInToBinary = (n) => {
+  let binary = "";
+  const integerBits = 32;
   if (numberIsZeroOrOne(n)) {
-    return n.toString();
-  } else return convertDecimalTo32BitBinary(n);
+    binary = n.toString();
+  } else binary = convertDecimalToBinary(n);
+  binary = "0".repeat(integerBits - binary.length) + binary;
+
+  return binary;
 };
 
-const binary = flipBits(3);
-console.log("🚀 ~ file: flippingbits.ts:38 ~ binary:", binary);
+const flipBinary = (binary) => {
+  let flipBits = "";
+
+  for (i = 0; i < binary.length; i++) {
+    if (binary[i] === "0") flipBits += "1";
+    else flipBits += "0";
+  }
+
+  return flipBits;
+};
+
+const convertBinaryToDecimal = (binary) => {
+  const digit = parseInt(binary, 2);
+  return digit;
+};
+
+const flipBits = (n) => {
+  const binary = convertInToBinary(n);
+  const flippedBinary = flipBinary(binary);
+  const flippedBinaryInDecimal = convertBinaryToDecimal(flippedBinary);
+  console.log("🚀 ~ file: flippingbits.js:54 ~ flipBits ~ flippedBinaryInDecimal:", flippedBinaryInDecimal)
+};
+
+const binary = flipBits(0);
+// console.log("🚀 ~ file: flippingbits.ts:38 ~ binary:", binary);
